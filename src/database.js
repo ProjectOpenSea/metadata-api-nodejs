89d9ee726dd6b2f801767eff86247528983d0a73
Mantas Stašauskas <mantas487@gmail.com>
2021-11-03 14:15:50 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   1: {
-    name: "Buzz",
+    name: "DoesTheChangeWork",
     description: "Buzzzzzz",
     image: "https://nfts-metadata.herokuapp.com/images/buzz.png",
     attributes: [
